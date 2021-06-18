0bd4a25b7355ff029b34775e8adb19880fd59881
Vikram <33194767+Vik0105@users.noreply.github.com>
2021-06-18 18:44:15 +0530

    store timestamp of data collection

--- storeTextFile.py
+++ storeTextFile.py
@@ -1,5 +1,7 @@
 # Store the values outputted by the weighing machine in a text file
+
 import serial
+from datetime import datetime
 
 # Connect Arduino to COM 1
 ser = serial.Serial('COM4', 9600, timeout=1)
@@ -13,5 +15,7 @@ while True
   # Decode the line from byte to string
   data = line.decode()
   data = data.partition("\")[0]
+  # Store the time of data collection
+  txtfile.write(datetime.now().strftime("%H:%M:%S") + "\n")
   # Write your data to the text file
   txtfile.write(data + "\n")
